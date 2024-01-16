a7aa442f8c58c01a3a011ace86aa3eb8d4e2cccf
Тимур Спектров <timurbadalbaev19@gmail.com>
2024-01-17 01:14:32 +0300

    3 fix

--- 3/src/index.js
+++
@@ -1,7 +0,0 @@
-const range = document.querySelector('.popup__range');
-const value = document.querySelector('.popup__value-number');
-
-range.addEventListener('input', (e) => {
-
-    value.textContent = e.target.value;
-})
